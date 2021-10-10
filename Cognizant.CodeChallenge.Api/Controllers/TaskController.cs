6c2519b8e4385cce61c26ccf7e39dac907aef530
vladimir_mit <vladimirmityah@gmail.com>
2021-10-10 16:10:21 +0300

    Add tasks controller

---
+++ Cognizant.CodeChallenge.Api/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cognizant.CodeChallenge.Application.Features.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Cognizant.CodeChallenge.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public Task<Get.Response> GetAll(int? skip, int? take, CancellationToken cancellationToken)
+        {
+            return _mediator.Send(new Get.Query(skip, take), cancellationToken);
+        }
+    }
+}
---
+++ Cognizant.CodeChallenge.Application/Features/Tasks/Get.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cognizant.CodeChallenge.Infrastructure.Database;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cognizant.CodeChallenge.Application.Features.Tasks
+{
+    public class Get
+    {
+        public class Query : IRequest<Response>
+        {
+            public Query(int? skip, int? take)
+            {
+                Skip = skip;
+                Take = take;
+            }
+
+            public int? Skip { get; }
+            public int? Take { get; set; }
+        }
+        
+        public class Response
+        {
+            public ICollection<TaskInfo> Result { get; }
+
+            public Response(ICollection<TaskInfo> result)
+            {
+                Result = result;
+            }
+        }
+        
+        public class Handler : IRequestHandler<Query, Response>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            
+            public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var taskQuery = _context.CodeTask.OrderBy(ct => ct.Name)
+                    .Select(ct => new TaskInfo { Id = ct.Id, Name = ct.Name, Description = ct.Description});
+
+                if (request.Skip is { })
+                    taskQuery = taskQuery.Skip(request.Skip.Value);
+
+                if (request.Take is { })
+                    taskQuery = taskQuery.Take(request.Take.Value);
+
+                var result = await taskQuery.ToListAsync(cancellationToken);
+
+                return new Response(result);
+            }
+        }
+        public class TaskInfo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
